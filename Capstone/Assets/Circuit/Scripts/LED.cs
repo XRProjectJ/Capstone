6a95ec6ce6acea00865a94a39107cf3cc8800596
estherP23 <otbanaba9763@naver.com>
2024-04-11 19:27:25 +0900

    led

--- Capstone/Assets/Circuit/Scripts/LED.cs
+++ Capstone/Assets/Circuit/Scripts/LED.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class LED : ComponentClass
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+         Do();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     public override void Do()
+    {
+        // off된 led 에셋을 찾음
+        GameObject offLed = GameObject.Find("LED Red");
+
+        // on된 led 에셋을 찾음
+        GameObject onLed = GameObject.Find("LED Red ON");
+
+        // off된 led를 비활성화
+        offLed.SetActive(false);
+
+        // on된 led를 활성화
+        onLed.SetActive(true);
+    }
+
 
 }
