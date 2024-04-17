29c00b455e894f707e077866985ac6993353c450
sum130 <sm3904@naver.com>
2024-04-17 17:59:48 +0900

    UISetting_ing

---
+++ Capstone/Assets/scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void Explain()
+    {
+
+    }
+
+    public void StartCircuit()
+    {
+
+    }
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
