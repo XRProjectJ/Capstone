2013f659b01a5a64e1891b5605ec2660f0de82ba
estherP23 <otbanaba9763@naver.com>
2024-05-08 20:36:59 +0900

    LED_전류 값에 따라 색상 조절

--- Capstone/Assets/Circuit/Scripts/LED.cs
+++ Capstone/Assets/Circuit/Scripts/LED.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class LED : ComponentClass
 {
     public GameObject material;
+    private Color originalColor;
 
     // Start is called before the first frame update
     void Start()
     {
-         Do();
+
+        // LED의 원래 색상 저장
+        originalColor = material.GetComponent<Renderer>().material.color;
+
+        material.GetComponent<Renderer>().material.color = new Color(0.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
@@ -18,24 +23,10 @@ public class LED : ComponentClass
         
     }
     
-     public override void Do()
+     public override void Do(double totalCurrent)
     {
-        /*// off된 led 에셋을 찾음
-        GameObject offLed = GameObject.Find("LED Red");
 
-        // on된 led 에셋을 찾음
-        GameObject onLed = GameObject.Find("LED Red ON");
-
-        // off된 led를 비활성화
-        offLed.SetActive(false);
-
-        // on된 led를 활성화
-        onLed.SetActive(true);*/
-
-        
-       
-
-        if (material != null)
+        /*if (material != null)
         {
 
             material.GetComponent<Renderer>().material.color = new Color(0.0f, 0.0f, 0.0f);
@@ -43,9 +34,35 @@ public class LED : ComponentClass
         else
         {
             Debug.LogWarning("Component_diode에 Material이 할당되지 않았습니다.");
-        }
-    
+        }*/
+
+        // 원래 색상으로 돌아가기
+        //material.GetComponent<Renderer>().material.color = originalColor;/
+
+        /*// 전류의 세기 가져오기
+        double current = GetI();
+
+        // 전류의 세기에 따라 LED의 색상 조절
+        Color newColor = originalColor * (float)current;
+        material.GetComponent<Renderer>().material.color = newColor;*/
+
+
+
+
+        // 부품 전류 
+        double componentCurrent = GetI();
+
+     
+        double ratio = componentCurrent / totalCurrent;
+
+        // LED의 색상 조절
+        Color newColor = originalColor * (float)ratio;
+
+        // LED 새로운 색상으로
+        material.GetComponent<Renderer>().material.color = newColor;
     }
 
-
 }
+
+
+
