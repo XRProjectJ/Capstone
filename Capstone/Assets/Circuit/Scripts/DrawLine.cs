ae70f0d7bac538f5c2d731e6cd1466d58de9dd1c
wrg2674 <wrg267499@naver.com>
2024-05-18 22:39:47 +0900

    Line 프리팹에 있던 pos의 Attachment 스크립트 삭제

--- Capstone/Assets/Circuit/Scripts/DrawLine.cs
+++ Capstone/Assets/Circuit/Scripts/DrawLine.cs
@@ -13,24 +13,26 @@ public class DrawLine : MonoBehaviour
     {
         lineRenderer.startWidth = 0.4f;
         lineRenderer.endWidth = 0.4f;
+        lineRenderer.widthMultiplier = 0.4f;
 
-    }
-    private void Draw()
-    {
-        midCp = (cp[0].transform.position + cp[1].transform.position) / 2 + new Vector3(0, 0.03f, 0);
-        lineRenderer.positionCount = pointCount;
-        Vector3[] pointPos = new Vector3[pointCount];
-        for(int i=0; i < pointCount; i++)
-        {
-            float t = i/(float)pointCount;
-            pointPos[i] = (1 - t) * (1 - t) * cp[0].transform.position + 2 * t * (1 - t) * midCp + t * t * cp[1].transform.position;
-            
-        }
-        lineRenderer.SetPositions(pointPos);
     }
     // Update is called once per frame
     void Update()
     {
         Draw();
+    }
+    private void Draw()
+    {
+        Vector3[] pointPos = new Vector3[pointCount];
+        midCp = (cp[0].transform.position + cp[1].transform.position) / 2 + new Vector3(0, 0.03f, 0);
+        lineRenderer.positionCount = pointCount;
+        for (int i = 0; i < pointCount; i++)
+        {
+            float t = i / (float)pointCount;
+            pointPos[i] = (1 - t) * (1 - t) * cp[0].transform.position + 2 * t * (1 - t) * midCp + t * t * cp[1].transform.position;
+
+        }
+        lineRenderer.SetPositions(pointPos);
+
     }
 }
