9d6b719c8f4ec2124a1bbc92fc6a0aa5456dc3fe
sum130 <sm3904@naver.com>
2024-05-27 21:16:14 +0900

    fix: 결과확인 이후, 부품별 정보 확인

--- Capstone/Assets/scripts/Battery_Value.cs
+++ Capstone/Assets/scripts/Battery_Value.cs
@@ -31,4 +31,21 @@ public class Battery_Value : MonoBehaviour
             Debug.LogError("Battery 스크립트를 찾을 수 없습니다.");
         }
     }
+
+    private void Update()
+    {
+        Power batteryScript = Battery_Object.GetComponent<Power>();
+        if (batteryScript != null)
+        {
+            // LED_Blue 스크립트에서 ShowR 값을 가져와서 UI Text에 표시
+            registance_text.text = "저항: " + batteryScript.GetShowR().ToString();
+            voltage_text.text = "전압: " + batteryScript.GetShowV().ToString();
+            intense_text.text = "전류: " + batteryScript.GetShowI().ToString();
+
+        }
+        else
+        {
+            Debug.LogError("Battery 스크립트를 찾을 수 없습니다.");
+        }
+    }
 }
--- Capstone/Assets/scripts/LED_Value.cs
+++ Capstone/Assets/scripts/LED_Value.cs
@@ -30,4 +30,23 @@ public class LED_Value : MonoBehaviour
             Debug.LogError("LED 스크립트를 찾을 수 없습니다.");
         }
     }
+    void Update()
+    {
+
+        LED ledScript = LED_Object.GetComponent<LED>();
+
+        if (ledScript != null)
+        {
+            // LED_Blue 스크립트에서 ShowR 값을 가져와서 UI Text에 표시
+            registance_text.text = "저항: " + ledScript.GetShowR().ToString();
+            voltage_text.text = "전압: " + ledScript.GetShowV().ToString();
+            intense_text.text = "전류: " + ledScript.GetShowI().ToString();
+
+        }
+        else
+        {
+            Debug.LogError("LED 스크립트를 찾을 수 없습니다.");
+        }
+    }
+
 }
